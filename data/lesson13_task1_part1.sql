c4bd8b08bbdbbcf39b8c6279879c88fac3ed9196
julija7249 <70820334+julija7249@users.noreply.github.com>
2024-06-06 23:19:51 +0300

    Update lesson13_task1_part1.sql

--- data/lesson13_task1_part1.sql
+++ data/lesson13_task1_part1.sql
@@ -1,6 +1,9 @@
-USE lesson12_task_2;
-CREATE TABLE Students (
-    Id INT IDENTITY(1, 1) PRIMARY KEY,
-    FirstName VARCHAR(100),
-    LastName VARCHAR(100)
-);
+USE DB_lesson;
+IF NOT EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'Students')
+BEGIN
+    CREATE TABLE Students (
+        Id INT PRIMARY KEY,
+        FirstName VARCHAR(100),
+        LastName VARCHAR(100)
+    );
+END
