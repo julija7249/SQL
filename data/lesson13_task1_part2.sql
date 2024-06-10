4dd1e6cf94ab025b859d057d7ef9dfb02b4a6fb4
Julija7249 <70820334+julija7249@users.noreply.github.com>
2024-06-10 23:31:06 +0300

    изменения lesson 13

---
+++ data/lesson13_task1_part2.sql
@@ -0,0 +1,10 @@
+USE t18;
+IF NOT EXISTS (
+    SELECT *
+    FROM INFORMATION_SCHEMA.COLUMNS
+    WHERE TABLE_NAME = 'Students' AND COLUMN_NAME = 'ExamScore'
+)
+BEGIN
+    ALTER TABLE Students
+    ADD ExamScore INT;
+END;
