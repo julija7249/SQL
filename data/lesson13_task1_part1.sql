9ef7d44c41642b35d788b94e08b14486ba9f9103
Julija7249 <70820334+julija7249@users.noreply.github.com>
2024-06-10 17:40:18 +0300

    новая версия lesson 13

--- data/lesson13_task1_part1.sql
+++ data/lesson13_task1_part1.sql
@@ -1,8 +1,8 @@
-USE DB_lesson;
+USE t18;
 IF NOT EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'Students')
 BEGIN
     CREATE TABLE Students (
-        Id INT PRIMARY KEY,
+        Id INT IDENTITY PRIMARY KEY,
         FirstName VARCHAR(100),
         LastName VARCHAR(100)
     );
--- data/lesson13_task1_part3.sql
+++ data/lesson13_task1_part3.sql
@@ -1,15 +1,27 @@
-USE DB_lesson;
-INSERT INTO Students (FirstName, LastName, ExamScore)
-SELECT FirstName, LastName, Score
-FROM (
-    VALUES 
+USE t17;
+IF NOT EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'ExamList')
+BEGIN
+    CREATE TABLE ExamList(
+		StudentNumber INT,
+		FirstName VARCHAR(100),
+		LastName VARCHAR(100),
+		ExamScore INT,
+		Direction VARCHAR(100),
+		FOREIGN KEY (StudentNumber)  REFERENCES Students (Id)
+	);
+END;
+IF NOT EXISTS (SELECT * FROM ExamList)
+BEGIN
+	INSERT INTO ExamList (FirstName, LastName, ExamScore, Direction)
+	VALUES
         ('Иван', 'Бунша', 220, 'История'),
         ('Остап', 'Бендер', 170, 'Право'),
-        ('Александр', 'Тимофеев', 300, 'Физика')
-) AS ExamList (FirstName, LastName, Score, Direction)
-WHERE NOT EXISTS (
-    SELECT 1
-    FROM Students
-    WHERE Students.FirstName = ExamList.FirstName
-    AND Students.LastName = ExamList.LastName
-);
+        ('Александр', 'Тимофеев', 300, 'Физика');
+END;
+IF NOT EXISTS (SELECT * FROM Students)
+BEGIN
+	INSERT INTO Students (FirstName, LastName, ExamScore) 
+	SELECT FirstName, LastName, ExamScore 
+	FROM ExamList;
+END;
+SELECT * FROM Students;
