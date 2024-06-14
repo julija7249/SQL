9a1951eb41560e1c4f134a739975e5ac49599fc4
Julija7249 <70820334+julija7249@users.noreply.github.com>
2024-06-14 13:09:40 +0300

    Добавлен lesson13_task2

---
+++ data/lesson13_task2.sql
@@ -0,0 +1,19 @@
+﻿﻿-- -*- coding: utf-8 -*-
+USE DB1;
+IF NOT EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'Directions')
+BEGIN
+    CREATE TABLE Directions(
+		CodeDirection INT PRIMARY KEY,
+		NameDirection VARCHAR(100),
+		MinMark INT
+	);
+END;
+IF NOT EXISTS (SELECT * FROM Directions)
+BEGIN
+	INSERT INTO Directions (CodeDirection, NameDirection, MinMark)
+	VALUES
+        (1, 'История', 160),
+        (2, 'Право', 180),
+        (3, 'Физика', 200);
+END;
+SELECT * FROM Directions;
