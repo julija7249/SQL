771e68898014b1e80e752559d1fdacdff17e9426
Julija7249 <70820334+julija7249@users.noreply.github.com>
2024-06-17 23:13:21 +0300

    добавлен min балл в lesson13_task3_part2

--- data/lesson13_task3_part2.sql
+++ data/lesson13_task3_part2.sql
@@ -3,6 +3,8 @@ UPDATE Students
 SET Students.CodeDirection = Directions.CodeDirection
 FROM Students
 JOIN ExamList ON ExamList.FirstName = Students.FirstName AND ExamList.LastName = Students.LastName
-JOIN Directions ON ExamList.Direction = Directions.NameDirection;
+JOIN Directions ON ExamList.Direction = Directions.NameDirection
+WHERE ExamList.ExamScore >= Directions.MinMark;
 
-SELECT * FROM Students;
+SELECT * FROM Students
+WHERE Students.CodeDirection IS NOT NULL;
