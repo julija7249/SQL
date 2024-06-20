8f9183175415b026fb71e26d3c03c7a60a6e716c
Julija7249 <70820334+julija7249@users.noreply.github.com>
2024-06-20 22:17:17 +0300

    Добавлен lesson14_task1

---
+++ query/lesson14_task1.sql
@@ -0,0 +1,6 @@
+USE DB_lesson;
+SELECT StudentNumber, FirstName, LastName, ExamScore, Direction,
+ IIF (ExamList.ExamScore >= Directions.MinMark, 'YES',
+ IIF (ExamList.ExamScore < Directions.MinMark, 'NO', Null)) AS Enrolled
+FROM ExamList
+JOIN Directions ON ExamList.Direction = Directions.NameDirection;
